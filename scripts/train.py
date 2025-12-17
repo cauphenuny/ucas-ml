763b230a11e2e8ca5abccff340f3f9ffea450c94
cauphenuny <starseekist@gmail.com>
2025-12-18 00:50:04 +0800

    feat: load pre-trained base model

--- scripts/train.py
+++ scripts/train.py
@@ -1,10 +1,12 @@
 # %%
 import pandas as pd
-import torch
 import argparse
 from tqdm import tqdm
 from loguru import logger
 from sklearn.metrics import classification_report, confusion_matrix
+from torch.utils.data import DataLoader
+from torch import load as torch_load
+from torch import save as torch_save
 
 # %%
 from app import dataloader
@@ -13,6 +15,8 @@ from app.classifier import TransformerClassifier
 from tinyllm.tokenize.tokenizer import Tokenizer
 from tinyllm.network.models import specifications as model_specs
 from tinyllm.network.multiplatform import ACCL_DEVICE
+from tinyllm.network.functional import cross_entropy
+from tinyllm.optimize.optimizers import AdamW
 
 # %%
 parser = argparse.ArgumentParser()
@@ -22,18 +26,38 @@ parser.add_argument("--lr", type=float, default=1e-4)
 parser.add_argument("--device", type=str, default=ACCL_DEVICE)
 parser.add_argument("--output_dir", type=str, default="output")
 parser.add_argument(
-    "--save_ckpt", type=str, default=None, help="Pathname to save the model checkpoint"
+    "--save_ckpt", type=str, default=None, help="Filename to save the model checkpoint"
 )
 parser.add_argument(
-    "--load_ckpt", type=str, default=None, help="Pathname to load the model checkpoint"
+    "--load_ckpt", type=str, default=None, help="Filename to load the model checkpoint"
+)
+parser.add_argument(
+    "--base_model", type=str, default=None, help="Path to base model checkpoint to load"
 )
 parser.add_argument(
     "--submit_file", type=str, default=None, help="Filename for submission"
 )
 parser.add_argument(
+    "--model_size",
+    type=str,
+    default="tiny",
+    help="Model size: nano, micro, tiny, small, medium, large, x-large, xx-large, 3x-large",
+    choices=[
+        "nano",
+        "micro",
+        "tiny",
+        "small",
+        "medium",
+        "large",
+        "x-large",
+        "xx-large",
+        "3x-large",
+    ],
+)
+parser.add_argument(
     "--reduction",
     type=str,
-    default="last",
+    default="first",
     help="Reduction method: mean, first, last",
     choices=["mean", "first", "last"],
 )
@@ -41,6 +65,18 @@ parser.add_argument(
     "--no-causal",
     action="store_true",
     help="Do not use causal masking in the transformer",
+)
+parser.add_argument(
+    "--tokenizer",
+    type=str,
+    default="movie-review",
+    help="Tokenizer name",
+)
+parser.add_argument(
+    "--vocab_size",
+    type=int,
+    default=5000,
+    help="Vocabulary size",
 )
 args = parser.parse_args()
 
@@ -51,16 +87,19 @@ if args.reduction in ("first", "mean"):
         logger.warning(
             "Causal masking is only compatible with 'last' reduction, auto set --no-causal to disable causal masking"
         )
-
+    if args.base_model is not None:
+        logger.warning(
+            "Base model loading is only compatible with 'last' reduction, ignoring --base_model"
+        )
+        args.base_model = None
+print(f"Parameters: {vars(args)}")
 # %%
 output_dir = PROJECT_ROOT / args.output_dir
 output_dir.mkdir(parents=True, exist_ok=True)
 
 # %%
-vocab_size = 5000
-tokenizer_name = "movie-review"
 tokenizer = Tokenizer.from_dir(
-    PROJECT_ROOT / "ckpts" / "tokenizer" / f"{tokenizer_name}-{vocab_size}"
+    PROJECT_ROOT / "ckpts" / "tokenizer" / f"{args.tokenizer}-{args.vocab_size}"
 )
 
 # %%
@@ -74,13 +113,13 @@ dataset = dataloader.Dataset(
     transform=dataloader.transform.to_tensor(tokenizer, device=args.device),
 )
 train, valid = dataset.split(test_size=0.2, random_state=42)
-train_dataloader = torch.utils.data.DataLoader(
+train_dataloader = DataLoader(
     train,
     batch_size=args.batch_size,
     shuffle=True,
     collate_fn=dataloader.transform.collate_padding(device=args.device),
 )
-valid_dataloader = torch.utils.data.DataLoader(
+valid_dataloader = DataLoader(
     valid,
     batch_size=args.batch_size,
     shuffle=False,
@@ -89,10 +128,11 @@ valid_dataloader = torch.utils.data.DataLoader(
 test = pd.read_csv(test_path, sep="\t", dtype=str, na_filter=False)
 
 # %%
-spec = model_specs("tiny")
+spec = model_specs(args.model_size)
+spec["share_embeddings"] = False
 num_classes = 5
 model = TransformerClassifier(
-    vocab_size=vocab_size,
+    vocab_size=args.vocab_size,
     context_length=256,
     num_classes=num_classes,
     causal=not args.no_causal,
@@ -101,8 +141,12 @@ model = TransformerClassifier(
 )
 print("Model architecture:")
 print(model)
+if args.base_model is not None:
+    print(f"Loading base model from {args.base_model}...")
+    base_state_dict = torch_load(args.base_model, map_location="cpu")
+    model.model.load_state_dict(base_state_dict, strict=False)
 model = model.to(args.device)
-criterion = torch.nn.CrossEntropyLoss()
+criterion = cross_entropy
 
 
 # %%
@@ -142,7 +186,13 @@ if args.load_ckpt is None:
         print(f"Training on device: {args.device}")
         for epoch in range(args.epoch):
             print(f"Starting epoch {epoch+1}/{args.epoch}...")
-            optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+            optimizer = AdamW(
+                model.parameters(),
+                lr=args.lr,
+                betas=(0.9, 0.999),
+                eps=1e-8,
+                weight_decay=0.01,
+            )
             model.train()
 
             ema_loss = 0.0
@@ -174,9 +224,9 @@ if args.load_ckpt is None:
         pdb.post_mortem()
 
     if args.save_ckpt is not None:
-        torch.save(model.state_dict(), output_dir / args.save_ckpt)
+        torch_save(model.state_dict(), output_dir / args.save_ckpt)
 else:
-    model.load_state_dict(torch.load(output_dir / args.load_ckpt))
+    model.load_state_dict(torch_load(output_dir / args.load_ckpt))
 
 # %%
 print("Final evaluation on validation set:")
