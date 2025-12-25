bfd2c984775ba8efb6a73dc83bdf6b896a9ef60b
cauphenuny <starseekist@gmail.com>
2025-12-25 16:04:37 +0800

    fix: add base class for wrapper

--- app/tokenizer/autotokenizer.py
+++ app/tokenizer/autotokenizer.py
@@ -1,6 +1,8 @@
 from transformers import AutoTokenizer
+from tinyllm.tokenize.tokenizer import Tokenizer
+from typing import override
 
-class AutoTokenizerWrapper:
+class AutoTokenizerWrapper(Tokenizer):
     def __init__(self, model_name: str):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
     
@@ -8,6 +10,7 @@ class AutoTokenizerWrapper:
     def from_pretrained(cls, model_name: str):
         return cls(model_name)
 
+    @override
     def encode(self, text: str) -> list[int]:
         tokens = self.tokenizer(text)
         return tokens['input_ids']
